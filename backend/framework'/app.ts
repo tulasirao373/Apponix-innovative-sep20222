bc99ca759a4f8ae232014bb79cf775bface49d32
tulasirao373 <117249394+tulasirao373@users.noreply.github.com>
2022-12-25 18:00:42 +0530

    video-img-path

--- backend/framework'/app.ts
+++ backend/framework'/app.ts
@@ -1,7 +1,12 @@
+
 var express=require('express')
 var app=express()
 var cors=require('cors')
 var bodyParser = require('body-parser')
+var path = require('path')
+var ejs = require('ejs')
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname, 'src/views'))
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -17,24 +22,28 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
-//it lookes at strtingand ending in between u can give anyththing
-// app.get('/c*ars',(req:any,res:any)=>{
-//     res.send('hello')
-// })
 
-// app.get('/ca?rs',(req:any,res:any)=>{
-//     res.send('hello')
-// })
-//multiple path
-// app.get('/api/cars',(req:any,res:any)=>{
-//     res.send('hello')
-// })
-// app.get('/ca+rs',(req:any,res:any)=>{
-//     res.send('hello')
-// })
-// app.get('/cars/:id/:name',(req:any,res:any)=>{
-//     res.send('hello')
-// })
+// app.get('/index',(req:any,res:any)=>{
+//     res.send('<h1>hello</h1>')
+//     })
+    
+//     app.get('/serverless',(req:any,res:any)=>{
+//         res.redirect('http://127.0.0.1:5500/test.html')
+//         })
+app.use(express.static(path.join(__dirname,'src/public/img')))
+app.use(express.static(path.join(__dirname,'src/public/videos')))
+
+
+app.route('/')
+.get((req: any, res: any) => {
+    res.render('home')
+}).post((req: any, res: any) => {
+    res.send('hello')
+}).delete((req: any, res: any) => {
+    res.send('hello')
+}).put((req: any, res: any) => {
+    res.send('hello')
+})
 
 app.listen(8003,()=>{
     console.log('server started on port no 8003')
