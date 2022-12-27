6d36117f9f93b255e8ca75f11af12fd9456a19a7
tulasirao373 <117249394+tulasirao373@users.noreply.github.com>
2022-12-27 18:21:38 +0530

    separation of concerns dotenv db integeration

--- backend/framework'/app.ts
+++ backend/framework'/app.ts
@@ -1,10 +1,13 @@
+require('dotenv').config()
+require("./config/db.config")
+import { express } from "./config/common.config"
 
-var express=require('express')
 var app=express()
 var cors=require('cors')
 var bodyParser = require('body-parser')
 var path = require('path')
 var ejs = require('ejs')
+var endpoint=require('./src/routes')
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname, 'src/views'))
 
@@ -33,18 +36,22 @@ app.use(cors())
 app.use(express.static(path.join(__dirname,'src/public/img')))
 app.use(express.static(path.join(__dirname,'src/public/videos')))
 
+app.use('/api',endpoint.homeRoute)
+
 
 app.route('/')
 .get((req: any, res: any) => {
-    res.render('home')
-}).post((req: any, res: any) => {
-    res.send('hello')
-}).delete((req: any, res: any) => {
-    res.send('hello')
-}).put((req: any, res: any) => {
-    res.send('hello')
+    res.send('welcome')
+})
+app.route('*')
+.get((req: any, res: any) => {
+
+    res.send({route:404})
 })
 
-app.listen(8003,()=>{
-    console.log('server started on port no 8003')
+
+
+
+app.listen(process.env.PORT, ()=> {
+    console.log('server started on port no ${process.env.PORT}')
 })
