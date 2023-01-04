d0e6497157f67d8aa387e14efa2bcd443da64568
tulasirao373 <117249394+tulasirao373@users.noreply.github.com>
2023-01-04 18:08:44 +0530

    worked on seller upload api

--- backend/framework'/src/controller/auth/login/login.Ctrl.ts
+++ backend/framework'/src/controller/auth/login/login.Ctrl.ts
@@ -1,16 +1,36 @@
+// import { userExits } from "../register"
+
+// const {UserRegInfo}=require('../../../models')
+// export const  userlogin_Post=(req:any,res:any)=>{
+// var email=req.body.email
+// var password=req.body.password
+// const logincallback=(data:boolean)=>{
+// if(data==true){
+//     console.log('login success')
+// }else{
+//     console.log('user do not exits')
+// }
+// }
+// userExits(email,logincallback)
+// res.status(200).send(200)
+// }  ex:-generic
+
+
+
+
+
+
+
 import { userExits } from "../register"
 
 const {UserRegInfo}=require('../../../models')
-export const  userlogin_Post=(req:any,res:any)=>{
+export const  userlogin_Post=async(req:any,res:any)=>{
 var email=req.body.email
 var password=req.body.password
-const logincallback=(data:boolean)=>{
-if(data==true){
-    console.log('login success')
+var userExits=await UserRegInfo.finOne({email:email})
+if(userExits==null){
+    res.send('register')
 }else{
-    console.log('user do not exits')
+    res.status(200).send(200)
 }
-}
-userExits(email,logincallback)
-res.status(200).send(200)
 }
--- backend/framework'/src/routes/index.ts
+++ backend/framework'/src/routes/index.ts
@@ -1,6 +1,7 @@
 const homeRoute=require('./home/home.routes')
 const register=require('./auth/registeration/userReg.routes')
 const userlogin=require('./auth/login/login.routes')
+const sellerUploadProduct=require('./seller/sellerUpload.routes')
 
 
 module.exports={homeRoute,register,userlogin}
